4d23df0d4194d3cbfdd9d22bbabdc4ee3086e4ed
Soptq <32592090+soptq@users.noreply.github.com>
2022-04-28 22:01:55 +0800

    fix moving average errors when data is not enough

--- src/history.ts
+++ src/history.ts
@@ -19,12 +19,16 @@ function sqrt(value: bigint) {
 }
 
 class GasHistory {
+    private globalMean: bigint = BigInt(0);
+    private globalVariance: bigint = BigInt(0);
+    private globalCount: number = 0;
     private storage: {
         [networkId: string]: {
             [address: string]: {
                 [signature: string]: {
                     mean: bigint,
                     variance: bigint
+                    count: number
                 }
             }
         };
@@ -34,6 +38,17 @@ class GasHistory {
         private alpha: number = Infinity,
     ) { }
 
+    getGlobalDistance(value: bigint) : bigint {
+        if (this.globalCount < 1 / this.alpha) {
+            return BigInt(0);
+        }
+        if (value > this.globalMean) {
+            return (value - this.globalMean) / sqrt(this.globalVariance);
+        } else {
+            return (this.globalMean - value) / sqrt(this.globalVariance);
+        }
+    }
+
     getDistance(
         networkId: string,
         address: string,
@@ -41,15 +56,15 @@ class GasHistory {
         value: bigint
     ): bigint {
         const entry = this.storage[networkId]?.[address]?.[signature];
-        if (!entry) {
+        // if the entry does not exist or the data is not enough, just return 0 as we are not sure
+        if (!entry || entry.count < 1 / this.alpha) {
             return BigInt(0);
         }
         const mean = entry.mean;
         const variance = entry.variance;
-        if (!mean && !variance) {
-            return BigInt(0)
-        }
-        if (mean && !variance) {
+
+        // if the variance is 0
+        if (!variance) {
             if (mean == value) {
                 return BigInt(0)
             }
@@ -62,12 +77,25 @@ class GasHistory {
         }
     }
 
+    globalEnqueue(value: bigint) {
+        this.globalCount += 1;
+        if (this.globalMean == BigInt(0)) {
+            this.globalMean = value;
+            this.globalVariance = BigInt(0);
+        } else {
+            const sigma = value - this.globalMean;
+            this.globalMean = this.globalMean + sigma * BigInt(this.alpha * 1e6) / BigInt(1e6);
+            this.globalVariance = (this.globalVariance + sigma * sigma * BigInt(this.alpha * 1e6) / BigInt(1e6)) * BigInt((1 - this.alpha) * 1e6) / BigInt(1e6);
+        }
+    }
+
     enqueue(
         networkId: string,
         address: string,
         signature: string,
         value: bigint
     ) {
+        this.globalEnqueue(value);
         if (!this.storage[networkId]) {
             this.storage[networkId] = {};
         }
@@ -77,18 +105,21 @@ class GasHistory {
         }
 
         if (this.storage[networkId][address][signature]) {
-            const {mean, variance} = this.storage[networkId][address][signature];
+            const {mean, variance, count} = this.storage[networkId][address][signature];
+            // this is calculations of the moving average and the moving variance
             const sigma = value - mean;
             const next_mean = mean + sigma * BigInt(this.alpha * 1e6) / BigInt(1e6);
             const next_variance = (variance + sigma * sigma * BigInt(this.alpha * 1e6) / BigInt(1e6)) * BigInt((1 - this.alpha) * 1e6) / BigInt(1e6);
             this.storage[networkId][address][signature] = {
                 mean: next_mean,
-                variance: next_variance
+                variance: next_variance,
+                count: count + 1
             };
         } else {
             this.storage[networkId][address][signature] = {
                 mean: value,
-                variance: BigInt(0)
+                variance: BigInt(0),
+                count: 1
             };
         }
     }
